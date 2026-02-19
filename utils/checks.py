75465add555fb1f0b13ebf51ac170ccd15687d47
alabdullakn <alabdulla8932@gmail.com>
2026-02-19 18:19:30 +0300

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -1,1 +1,12 @@
+def has_bad_runs(seq: str, max_run: int = 4) -> bool:
+    seq = seq.upper()
+    run = 1
+    for i in range(1, len(seq)):
+        if seq[i] == seq[i - 1]:
+            run += 1
+            if run > max_run:
+                return True
+        else:
+            run = 1
+    return False
 
