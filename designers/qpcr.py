ee372134b17e84aaf4c949a78498bd380c9e4fb7
alabdullakn <alabdulla8932@gmail.com>
2026-02-19 18:56:28 +0300

    Update qpcr.py

--- designers/qpcr.py
+++ designers/qpcr.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-from utils import clean_dna, gc_pct, tm_wallace, revcomp, primer_score
-from utils.blast import primer_blast_url_pair
+from primer_engine import design_exon_primers, print_dimer_report
+from utils.checks import primer_blast_url_pair, primer_blast_url_single
 from ui.text import BLAST_INSTRUCTIONS, SCORE_EXPLANATION
+from ui.footer import add_footer
+
 
 
 import streamlit as st
