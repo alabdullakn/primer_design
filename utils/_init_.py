295663fda118b4afa2e059725005ac705cd92133
alabdullakn <alabdulla8932@gmail.com>
2026-02-19 18:53:07 +0300

    Update _init_.py

--- utils/_init_.py
+++ utils/_init_.py
@@ -1,5 +1,5 @@
 from .blast import primer_blast_url_pair, primer_blast_url_single
-from .seq import clean_dna, gc_pct, tm_wallace, revcomp, has_bad_runs
+from .seq import clean_dna, gc_pct, tm_wallace, revcomp, primer_score
 from .scoring import primer_score
 from .tm import tm_wallace
 from .checks import has_bad_runs
