207a66dda41d125845b46e0442e6ef21c91cf98b
alabdullakn <alabdulla8932@gmail.com>
2026-02-19 19:02:32 +0300

    Update _init_.py

--- utils/_init_.py
+++ utils/_init_.py
@@ -3,4 +3,5 @@ from .seq import clean_dna, gc_pct, tm_wallace, revcomp, primer_score
 from .scoring import primer_score
 from .tm import tm_wallace
 from .checks import has_bad_runs
+from .primer_utils import clean_dna, gc_pct, tm_wallace, revcomp, primer_score
 
