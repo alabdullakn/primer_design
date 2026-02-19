140a1d9845125d1fd91da0854622469c773f716f
alabdullakn <alabdulla8932@gmail.com>
2026-02-19 18:56:52 +0300

    Update splicing.py

--- designers/splicing.py
+++ designers/splicing.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-from utils import clean_dna, revcomp, gc_pct, tm_wallace, has_bad_runs
-from utils.blast import primer_blast_url_pair, primer_blast_url_single
+from primer_engine import design_exon_primers, print_dimer_report
+from utils.checks import primer_blast_url_pair, primer_blast_url_single
 from ui.text import BLAST_INSTRUCTIONS, SCORE_EXPLANATION
+from ui.footer import add_footer
+
 
 import streamlit as st
 
