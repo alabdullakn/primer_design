76e0ab3a0e0344a967a489b003f29ef9cf34e359
alabdullakn <alabdulla8932@gmail.com>
2026-02-19 18:33:50 +0300

    Update qpcr.py

--- designers/qpcr.py
+++ designers/qpcr.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import pandas as pd
+
+from utils import clean_dna, revcomp, gc_pct, tm_wallace, has_bad_runs
+from utils.blast import primer_blast_url_pair, primer_blast_url_single
+from ui.blocks import BLAST_INSTRUCTIONS, SCORE_EXPLANATION, add_footer
+
+
+import streamlit as st
 
 def render():
     st.title("qPCR primers")
