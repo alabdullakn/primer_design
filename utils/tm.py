d91175c0503be31f122bf00012bcbb93b1fd1100
alabdullakn <alabdulla8932@gmail.com>
2026-02-19 18:19:11 +0300

    Update tm.py

--- utils/tm.py
+++ utils/tm.py
@@ -1,1 +1,8 @@
+def tm_wallace(seq: str) -> float:
+    seq = seq.upper()
+    a = seq.count("A")
+    t = seq.count("T")
+    g = seq.count("G")
+    c = seq.count("C")
+    return 2.0 * (a + t) + 4.0 * (g + c)
 
