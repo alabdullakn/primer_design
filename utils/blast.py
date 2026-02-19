1873c9b66c0ae048e97fce2df4ef9e074fab7e4a
alabdullakn <alabdulla8932@gmail.com>
2026-02-19 18:43:13 +0300

    Update blast.py

--- utils/blast.py
+++ utils/blast.py
@@ -4,13 +4,13 @@ def primer_blast_url_pair(fwd_seq: str, rev_seq: str, organism: str = "Homo sapi
     params = {
         "PRIMER_LEFT_INPUT": fwd_seq,
         "PRIMER_RIGHT_INPUT": rev_seq,
-        "ORGANISM": organism
+        "ORGANISM": organism,
     }
     return "https://www.ncbi.nlm.nih.gov/tools/primer-blast/index.cgi?" + urllib.parse.urlencode(params)
 
 def primer_blast_url_single(primer_seq: str, organism: str = "Homo sapiens") -> str:
     params = {
         "PRIMER_LEFT_INPUT": primer_seq,
-        "ORGANISM": organism
+        "ORGANISM": organism,
     }
     return "https://www.ncbi.nlm.nih.gov/tools/primer-blast/index.cgi?" + urllib.parse.urlencode(params)
