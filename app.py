2eee062b41c2b7ede52b51785aa61a575f704568
alabdullakn <alabdulla8932@gmail.com>
2026-02-22 18:39:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,22 +5,27 @@ apply_styles()
 import streamlit as st
 from designers.regular_pcr import render as render_regular
 from designers.splicing import render as render_splicing
+from designers.home import render as render_home
 
-st.set_page_config(page_title="Primer Designer", layout="wide")
+st.set_page_config(page_title="Qprimer", layout="wide")
 apply_styles()
 
-tabs = st.tabs(["Regular PCR", "Splicing primers", "qPCR primers"])
+tabs = st.tabs(["Home", "Regular PCR", "Splicing primers", "qPCR primers"])
 
 with tabs[0]:
-    render_regular()
+    render_home()
 
 with tabs[1]:
-    render_splicing()
+    render_regular()
 
 with tabs[2]:
+    render_splicing()
+
+with tabs[3]:
     try:
         from designers.qpcr import render as render_qpcr
         render_qpcr()
     except Exception as e:
         st.error("qPCR tab crashed during import or rendering.")
         st.exception(e)
+designers/home.py
